b25dfb4f15ced96665f9f23deda7e5a323f1364a
ZEYAD MUHAMMED <81055759+ZeyadM7MD@users.noreply.github.com>
2021-11-27 20:39:30 +0200

    updated

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import store from "./store";
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(store).mount("#app");
